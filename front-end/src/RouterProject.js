68da8a494a6870bbf0ef24bad430cfccb23e2cbc
YakubOzten <104498397+YakubOzten@users.noreply.github.com>
2023-11-09 02:06:22 +0300

    Front-end Created

---
+++ front-end/src/RouterProject.js
@@ -0,0 +1,66 @@
+// rfce
+// React
+import React from 'react'
+
+//Router
+import { Navigate, Route, Routes } from 'react-router-dom'
+
+// Header Main Footer
+import HeaderProject from './Components/HeaderProject'
+import MainProject from './Components/MainProject'
+import FooterProject from './Components/FooterProject'
+
+// login
+import Login from './components/login/Login'
+
+//register
+import RegisterCreate from './Components/register/RegisterCreate'
+import RegisterUpdate from './Components/register/RegisterUpdate'
+import RegisterView from './Components/register/RegisterView'
+import RegisterList from './Components/register/RegisterList'
+
+// user
+import User from './Components/user/User'
+
+
+// FUNCTION COMPONENT
+function RouterProject() {
+
+    // RETURN
+    return (
+        <React.Fragment>
+            <HeaderProject logo="" />
+
+            <div className="container mt-5 App-header">
+                <Routes>
+
+                    {/* ROOT */}
+                    <Route path="/" element={<MainProject />} />
+                    <Route path="/index" element={<MainProject />} />
+
+                    {/* LOGIN */}
+                    <Route path="/login" element={<Login />} />
+
+                    {/* REGISTER */}
+                    <Route path="/register/create" element={<RegisterCreate />} />
+                    <Route path="/register/list" element={<RegisterList />} />
+                    <Route path="/register/view/:id" element={<RegisterView />} />
+                    <Route path="/register/update/:id" element={<RegisterUpdate />} />
+
+                    {/* USER PAGE */}
+                    <Route path="/user" element={<User />} />
+
+                    {/* Bad request */}
+                    <Route path={"*"} element={<Navigate to={"/"} />} />
+
+                </Routes>
+            </div>
+
+            <FooterProject copy="&copy; Bütün Haklar saklıdır." />
+        </React.Fragment>
+    ) //end return
+} // end function 
+
+
+// EXPORT
+export default RouterProject
---
+++ front-end/src/index.js
@@ -0,0 +1,30 @@
+// React
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import reportWebVitals from './reportWebVitals';
+
+// index. css (Dark mode)
+import './index.css';
+
+// Router Project
+import RouterProject from './RouterProject';
+
+// Dil için
+import './internationalization/i18nlanguage'
+
+// Browser Router
+// BrowserRouter: http://localhost:3000
+// HashRouter: http://localhost:3000/#/
+import { BrowserRouter } from 'react-router-dom';
+
+// ROOT-DOM
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <RouterProject />
+    </BrowserRouter>
+  </React.StrictMode>
+);
+
+reportWebVitals();
